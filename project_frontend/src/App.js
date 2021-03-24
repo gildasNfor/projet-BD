abcae5a8432b736549923f575a654c63c27e5a0d
Myst <cadellgriffith@gmail.com>
2021-03-24 08:08:10 +0100

    first-commit

---
+++ project_frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Center from './components/Center';
+
+function App() {
+  return (
+    <div>
+      <Center/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project_frontend/src/components/Center.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Center() {
+    return (
+        <div className = "center">
+            <h1> this is the tontine app</h1>
+        </div>
+    )
+}
+
+export default Center

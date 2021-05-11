66bff0feefd99e7bb94af2ad10baa2ee0cda440c
Myst <cadellgriffith@gmail.com>
2021-05-11 15:33:22 +0100

    cleaning backend

--- project_frontend/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-
-const allReducers = combineReducers({
-  auth: authReducer,
-});
-
-export default allReducers;

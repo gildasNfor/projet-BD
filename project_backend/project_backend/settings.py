e5ac15498e8c09fdf570b73fa03a6c71198c03d1
Myst-237 <66015538+Myst-237@users.noreply.github.com>
2021-12-07 16:25:50 +0100

    Update settings.py

--- project_backend/project_backend/settings.py
+++ project_backend/project_backend/settings.py
@@ -175,4 +175,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cadellgriffith@gmail.com'
-EMAIL_HOST_PASSWORD = 'kalamai2'
+EMAIL_HOST_PASSWORD = '********'
